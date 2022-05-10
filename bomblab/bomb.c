2c4d686114e2767a7500e28f98dd551f3f04d8b2
Grant Pauker <grantpauker@gmail.com>
2022-05-10 16:01:59 -0700

    Format code

--- bomblab/bomb.c
+++ bomblab/bomb.c
@@ -13,17 +13,16 @@
  * by law.
  ***************************************************************************/
 
+#include "phases.h"
+#include "support.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "support.h"
-#include "phases.h"
 
-/* 
+/*
  * Note to self: Remember to erase this file so my victims will have no
  * idea what is going on, and so they will all blow up in a
- * spectaculary fiendish explosion.  ***giggles*** 
+ * spectaculary fiendish explosion.  ***giggles***
  */
-
 
 FILE *infile;
 
@@ -31,30 +30,34 @@ int main(int argc, char *argv[])
 {
     char *input;
 
-    /* Note to self: remember to port this bomb to Windows and put a 
+    /* Note to self: remember to port this bomb to Windows and put a
      * fantastic GUI on it. */
 
-    /* When run with no arguments, the bomb reads its input lines 
+    /* When run with no arguments, the bomb reads its input lines
      * from standard input. */
-    if (argc == 1) {  
-	infile = stdin;
-    } 
+    if (argc == 1)
+    {
+        infile = stdin;
+    }
 
-    /* When run with one argument <file>, the bomb reads from <file> 
-     * until EOF, and then switches to standard input. Thus, as you 
+    /* When run with one argument <file>, the bomb reads from <file>
+     * until EOF, and then switches to standard input. Thus, as you
      * defuse each phase, you can add its defusing string to <file> and
      * avoid having to retype it. */
-    else if (argc == 2) {
-	if (!(infile = fopen(argv[1], "r"))) {
-	    printf("%s: Error: Couldn't open %s\n", argv[0], argv[1]);
-	    exit(8);
-	}
+    else if (argc == 2)
+    {
+        if (!(infile = fopen(argv[1], "r")))
+        {
+            printf("%s: Error: Couldn't open %s\n", argv[0], argv[1]);
+            exit(8);
+        }
     }
 
     /* You can't call the bomb with more than 1 command line argument. */
-    else {
-	printf("Usage: %s [<input_file>]\n", argv[0]);
-	exit(8);
+    else
+    {
+        printf("Usage: %s [<input_file>]\n", argv[0]);
+        exit(8);
     }
 
     /* Do all sorts of secret stuff that makes the bomb harder to defuse. */
@@ -63,10 +66,10 @@ int main(int argc, char *argv[])
     welcome_message(argc);
 
     /* Hpmh, some students ... I bet they can be stopped with a single character */
-    input = read_line();             /* Get input                   */
-    phase_1(input);                  /* Run the phase               */
-    phase_defused();                 /* Drat!  They figured it out!
-				      * Let me know how they did it. */
+    input = read_line(); /* Get input                   */
+    phase_1(input);      /* Run the phase               */
+    phase_defused();     /* Drat!  They figured it out!
+                          * Let me know how they did it. */
     printf("Phase 1 defused. And they said you'd never amount to anything. \n");
 
     /* Looks like we got a smart one.  In that *CASE*, time to *SWITCH* it up a little! */
@@ -102,6 +105,6 @@ int main(int argc, char *argv[])
     printf("Now that, was beautiful!\n");
     /* Hehe, they think they solved it... I'm sure no cs 33 student
      * can find my s3crets ...  */
-     
+
     return 0;
 }
